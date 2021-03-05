fafd2d14caf1a6a289ce996340121e06ece10a13
icik251 <hristostanulov@gmail.com>
2021-03-05 14:31:14 +0100

    Multiple outputs and changed architecture

--- chess_evaluation_model.py
+++ chess_evaluation_model.py
@@ -6,57 +6,89 @@ import numpy as np
 class ChessEvaluationModel:
     def __init__(self, bitmap_shape: tuple, additional_features_shape: tuple) -> None:
         # Init model
-        self.model = ChessEvaluationModel.__create_model(bitmap_shape, additional_features_shape)
+        self.model = ChessEvaluationModel.__create_model(
+            bitmap_shape, additional_features_shape
+        )
 
     @staticmethod
     def __create_model(bitmap_shape, additional_features_shape) -> models.Model:
         # define the inputs
         input_cnn = Input(shape=bitmap_shape)
         input_numerical = Input(shape=additional_features_shape)
-        # CNN
-        conv_1 = layers.Conv2D(32, kernel_size=(3, 3), strides=(1, 1), activation="relu")(input_cnn)
-        # Do we want max pooling?
-        max_1 = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv_1)
-        conv_2 = layers.Conv2D(64, kernel_size=(3, 3), strides=(1, 1), activation="relu")(max_1)
-        # Do we want max pooling?
-        max_2 = layers.MaxPooling2D(pool_size=(2, 2))(conv_2)
-        # Flatten data to allow concatenation with numerical feature vector
-        flatten = layers.Flatten()(max_2)
-        # Reduce the dimensionality before concatenating
-        dense_1 = layers.Dense(100, activation="relu")(flatten)
-        merged_layer = keras.layers.concatenate([dense_1, input_numerical])
-        # Output evaluation of position
-        output = layers.Dense(2, activation="linear")(merged_layer)
 
-        return models.Model(inputs=[input_cnn, input_numerical], outputs=output)
+        # Marco Wiering paper architecture
+        conv_1 = layers.Conv2D(
+            20, kernel_size=(5, 5), strides=(1, 1), activation="elu"
+        )(input_cnn)
+        batch_norm_1 = layers.BatchNormalization()(conv_1)
+        dropout_1 = layers.Dropout(0.3)(batch_norm_1)
+
+        # Do we want max pooling? - FOR NOW, NO as we want to preserve the whole information
+        # max_1 = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv_1)
+
+        conv_2 = layers.Conv2D(
+            50, kernel_size=(3, 3), strides=(1, 1), activation="elu"
+        )(dropout_1)
+        batch_norm_2 = layers.BatchNormalization()(conv_2)
+        dropout_2 = layers.Dropout(0.3)(batch_norm_2)
+
+        # Do we want max pooling? - FOR NOW, NO as we want to preserve the whole information
+        # max_2 = layers.MaxPooling2D(pool_size=(2, 2))(conv_2)
+
+        # Flatten data to allow concatenation with numerical feature vector
+        flatten = layers.Flatten()(dropout_2)
+        # Reduce the dimensionality before concatenating
+        dense_1 = layers.Dense(500, activation="elu")(flatten)
+        merged_layer = keras.layers.concatenate([dense_1, input_numerical])
+        batch_norm_3 = layers.BatchNormalization()(merged_layer)
+        dropout_3 = layers.Dropout(0.3)(batch_norm_3)
+
+        # Output evaluation of position
+        output_eval = layers.Dense(1, activation="linear")(dropout_3)
+        # Output number of turns to forced mate
+        output_mate = layers.Dense(1, activation="linear")(dropout_3)
+        # Output binary representing eval (0) or mate (1)
+        output_binary = layers.Dense(1, activation="sigmoid")(dropout_3)
+
+        return models.Model(
+            inputs=[input_cnn, input_numerical],
+            outputs=[output_eval, output_mate, output_binary],
+        )
 
     def get_summary(self) -> str:
         return self.model.summary()
 
-    def compile(self, optimizer: str, loss: str) -> None:
-        self.model.compile(optimizer=optimizer, loss=loss)
+    def compile(
+        self,
+        optimizer: str = "SGD",
+        loss: str = "root_mean_squared_error",
+        metrics: list = [
+            "root_mean_squared_error"
+        ],  # list of metrics to evaluate model
+    ) -> None:
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
     def train(
         self,
-        train_data: list,  # list with 2 elements: [cnn_features, numerical_features]
-        train_target: np.ndarray,
-        epochs: int,
-        batch_size: int,
+        train_data: list,  # list with 2 elements: [cnn_features, additional_features]
+        train_target: list,  # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
+        epochs: int = 100,
+        batch_size: int = 128,
     ) -> dict:
         return self.model.fit(
             train_data,
             train_target,
             epochs=epochs,
             validation_split=0.1,
-            batch_size=batch_size
+            batch_size=batch_size,
         )
 
     def train_validate(
         self,
-        train_data: list,  # list with 2 elements: [cnn_features, numerical_features]
-        train_target: np.ndarray,
-        val_data: list,  # list with 2 elements: [cnn_features, numerical_features]
-        val_target: np.ndarray,
+        train_data: list,  # list with 2 elements: [cnn_features, additional_features]
+        train_target: list,  # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
+        val_data: list,  # list with 2 elements: [cnn_features, additional_features]
+        val_target: list,  # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
         epochs: int,
         batch_size: int,
     ) -> dict:
@@ -65,7 +97,7 @@ class ChessEvaluationModel:
             train_target,
             epochs=epochs,
             validation_data=(val_data, val_target),
-            batch_size=batch_size
+            batch_size=batch_size,
         )
 
 
