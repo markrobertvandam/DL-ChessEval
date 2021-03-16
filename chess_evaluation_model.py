Reworked early stopping, changed input scaling, removed batchnorm, fixed output metrics, implemented model saving

chess_evaluation_model.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras import layers, models, Input
 from tensorflow.python.keras.callbacks import History, EarlyStopping
+
+from custom_early_stopping import CustomEarlyStopping
 from data_processing import DataProcessing
 from sklearn.metrics import mean_squared_error, accuracy_score
 
@@ -22,8 +24,7 @@ class ChessEvaluationModel:
         conv_1 = layers.Conv2D(
             20, kernel_size=(5, 5), strides=(1, 1), activation="elu"
         )(input_cnn)
-        batch_norm_1 = layers.BatchNormalization()(conv_1)
-        dropout_1 = layers.Dropout(0.3)(batch_norm_1)
+        dropout_1 = layers.Dropout(0.3)(conv_1)
 
         # Do we want max pooling? - FOR NOW, NO as we want to preserve the whole information
         # max_1 = layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(conv_1)
@@ -31,8 +32,7 @@ class ChessEvaluationModel:
         conv_2 = layers.Conv2D(
             50, kernel_size=(3, 3), strides=(1, 1), activation="elu"
         )(dropout_1)
-        batch_norm_2 = layers.BatchNormalization()(conv_2)
-        dropout_2 = layers.Dropout(0.3)(batch_norm_2)
+        dropout_2 = layers.Dropout(0.3)(conv_2)
 
         # Do we want max pooling? - FOR NOW, NO as we want to preserve the whole information
         # max_2 = layers.MaxPooling2D(pool_size=(2, 2))(conv_2)
@@ -42,20 +42,19 @@ class ChessEvaluationModel:
         # Reduce the dimensionality before concatenating
         dense_1 = layers.Dense(1000, activation="elu")(flatten)
         merged_layer = keras.layers.concatenate([dense_1, input_numerical])
-        batch_norm_3 = layers.BatchNormalization()(merged_layer)
-        # dropout_3 = layers.Dropout(0.5)(batch_norm_3)
+        dropout_3 = layers.Dropout(0.3)(merged_layer)
 
         # Output evaluation of position
-        output_eval = layers.Dense(1, activation="linear", name="output_eval")(
-            batch_norm_3
+        output_eval = layers.Dense(1, activation="linear", name="eval_score")(
+            dropout_3
         )
         # Output number of turns to forced mate
-        output_mate = layers.Dense(1, activation="linear", name="output_mate")(
-            batch_norm_3
+        output_mate = layers.Dense(1, activation="linear", name="mate_turns")(
+            dropout_3
         )
         # Output binary representing eval (0) or mate (1)
-        output_binary = layers.Dense(1, activation="sigmoid", name="output_binary")(
-            batch_norm_3
+        output_binary = layers.Dense(1, activation="sigmoid", name="is_mate")(
+            dropout_3
         )
 
         return models.Model(
@@ -67,24 +66,17 @@ class ChessEvaluationModel:
     def plot_history(history, plot_path: str):
         history = history.history
         # plot the training loss and accuracy
-        n = np.arange(0, len(history["loss"]))
+        n = np.arange(1, len(history["loss"]))
         plt.style.use("ggplot")
         plt.figure()
-        plt.plot(n, history["loss"], label="overall_train_loss")
-        plt.plot(n, history["output_eval_loss"], label="train_eval_loss")
-        plt.plot(n, history["output_mate_loss"], label="train_mate_loss")
-        plt.plot(
-            n, history["output_binary_binary_accuracy"], label="train_is_mate_accuracy"
-        )
+
+        plt.plot(n, history["eval_score_loss"][1:], label="train_eval_loss")
+        plt.plot(n, history["mate_turns_loss"][1:], label="train_mate_loss")
+
         if "val_loss" in history:
-            plt.plot(n, history["val_loss"], label="overall_val_loss")
-            plt.plot(n, history["val_output_eval_loss"], label="val_eval_loss")
-            plt.plot(n, history["val_output_mate_loss"], label="val_mate_loss")
-            plt.plot(
-                n,
-                history["val_output_binary_binary_accuracy"],
-                label="val_is_mate_accuracy",
-            )
+            plt.plot(n, history["val_eval_score_loss"][1:], label="val_eval_loss")
+            plt.plot(n, history["val_mate_turns_loss"][1:], label="val_mate_loss")
+
         plt.title("Training Loss and Accuracy")
         plt.xlabel("Epoch #")
         plt.ylabel("Loss/Accuracy")
@@ -95,20 +87,34 @@ class ChessEvaluationModel:
         return self.model.summary()
 
     def initialize(
-        self,
-        bitmap_shape: tuple,
-        additional_features_shape: tuple,
-        optimizer: str = "SGD",
-        loss: dict = {
-            "output_eval": "mean_squared_error",
-            "output_mate": "mean_squared_error",
-            "output_binary": "binary_crossentropy",
-        },  # Dict with key the name of the output layer and value the loss function
-        loss_weights: list = None,  # We can specify different weight for each loss
-        metrics: list = None,  # list of metrics to evaluate model
-        path_to_scalers: str = None,  # path to scalers
+            self,
+            bitmap_shape: tuple,
+            additional_features_shape: tuple,
+            optimizer="SGD",
+            loss=None,  # Dict with key the name of the output layer and value the loss function
+            loss_weights: list = None,  # We can specify different weight for each loss
+            metrics: list = None,  # list of metrics to evaluate model
+            path_to_scalers: str = None,  # path to scalers
     ) -> None:
 
+        if loss is None:
+            loss = {
+                "eval_score": "mean_squared_error",
+                "mate_turns": "mean_squared_error",
+                "is_mate": "binary_crossentropy",
+            }
+        if metrics is None:
+            metrics = {
+                "eval_score": "mean_squared_error",
+                "mate_turns": "mean_squared_error",
+                "is_mate": "binary_accuracy",
+            }
+        # if loss_weights is None:
+        #     loss_weights = {
+        #         "eval_score": 1,
+        #         "mate_turns": 1,
+        #         "is_mate": 0.1,
+        #     }
         self.model = self.__create_model(bitmap_shape, additional_features_shape)
         self.model.compile(
             optimizer=optimizer, loss=loss, metrics=metrics, loss_weights=loss_weights
@@ -121,11 +127,12 @@ class ChessEvaluationModel:
             self.data_processing_obj.load_scalers(path=path_to_scalers)
 
     def train_redundant(
-        self,
-        train_data: list,  # list with 2 elements: [cnn_features, additional_features]
-        train_target: list,  # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
-        epochs: int = 100,
-        batch_size: int = 128,
+            self,
+            train_data: list,  # list with 2 elements: [cnn_features, additional_features]
+            train_target: list,
+            # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
+            epochs: int = 100,
+            batch_size: int = 128,
     ) -> History:
         return self.model.fit(
             train_data,
@@ -136,13 +143,15 @@ class ChessEvaluationModel:
         )
 
     def train_validate(
-        self,
-        train_data: list,  # list with 2 elements: [cnn_features, additional_features]
-        train_target: list,  # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
-        val_data: list,  # list with 2 elements: [cnn_features, additional_features]
-        val_target: list,  # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
-        epochs: int = 100,
-        batch_size: int = 128,
+            self,
+            train_data: list,  # list with 2 elements: [cnn_features, additional_features]
+            train_target: list,
+            # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
+            val_data: list,  # list with 2 elements: [cnn_features, additional_features]
+            val_target: list,
+            # list with 3 elements: [position_eval, num_turns_to_mate, binary for eval (0) or mate (1)]
+            epochs: int = 100,
+            batch_size: int = 128,
     ) -> dict:
 
         train_eval_reshaped = train_target[0].reshape(-1, 1)
@@ -173,11 +182,7 @@ class ChessEvaluationModel:
         train_target = [train_eval_normalized, train_mate_normalized, train_target[2]]
         val_target = [val_eval_normalized, val_mate_normalized, val_target[2]]
 
-        es = EarlyStopping(monitor = 'val_loss',
-                           mode = 'min',
-                           patience = 10,
-                           min_delta = 0.005, 
-                           restore_best_weights = True)
+        es = CustomEarlyStopping(patience=10, d_eval=1e-2, d_mate=2e-4)
         return self.model.fit(
             train_data,
             train_target,
@@ -223,3 +228,6 @@ class ChessEvaluationModel:
 
     def predict(self, data):
         return self.model.predict(data)
+
+    def save_model(self, model_path):
+        self.model.save(model_path)

data_processing.py
@@ -6,7 +6,7 @@ import os
 
 class DataProcessing:
     def __init__(
-        self, scaler_type_eval: str = "min_max", scaler_type_mate="min_max"
+            self, scaler_type_eval: str = "min_max", scaler_type_mate="min_max"
     ) -> None:
         self.scaler_type_eval = scaler_type_eval
         self.scaler_type_mate = scaler_type_mate
@@ -16,16 +16,16 @@ class DataProcessing:
 
     def init_scalers(self) -> None:
         if self.scaler_type_eval == "min_max":
-            self.scaler_eval = MinMaxScaler()
+            self.scaler_eval = MinMaxScaler((0, 100))
         elif self.scaler_type_eval == "standard":
             self.scaler_eval = StandardScaler()
         if self.scaler_type_mate == "min_max":
-            self.scaler_mate = MinMaxScaler()
+            self.scaler_mate = MinMaxScaler((0, 100))
         elif self.scaler_type_mate == "standard":
             self.scaler_mate = StandardScaler()
 
     def fit_scalers(
-        self, train_eval_target: np.ndarray, train_mate_target: np.ndarray
+            self, train_eval_target: np.ndarray, train_mate_target: np.ndarray
     ) -> None:
         self.scaler_eval.fit(train_eval_target)
         self.scaler_mate.fit(train_mate_target)
@@ -68,7 +68,7 @@ class DataProcessing:
             print("No scalers found in the given path.")
 
     def train_val_test_split(
-        self, bitmaps, attributes, labels, train_split=0.8, val_split=0.1
+            self, bitmaps, attributes, labels, train_split=0.8, val_split=0.1
     ) -> tuple:
         train_bitmaps, val_bitmaps, test_bitmaps = self.split_logic(
             bitmaps, train_split, val_split
@@ -94,9 +94,7 @@ class DataProcessing:
 
     def split_logic(self, data, train_split=0.8, val_split=0.1) -> tuple:
         train_data = data[: int(len(data) * train_split)]
-        val_data = data[
-            int(len(data) * train_split) : int(len(data) * (train_split + val_split))
-        ]
-        test_data = data[int(len(data) * (train_split + val_split)) :]
+        val_data = data[int(len(data) * train_split): int(len(data) * (train_split + val_split))]
+        test_data = data[int(len(data) * (train_split + val_split)):]
 
         return train_data, val_data, test_data

main.py
@@ -8,18 +8,34 @@ from chess_evaluation_model import ChessEvaluationModel
 from data_processing import DataProcessing
 
 
+def gpu_fix() -> None:
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
 def main():
+    gpu_fix()
     parser = argparse.ArgumentParser(description="Preprocess the chess dataset")
-    parser.add_argument("--bitmaps", help="Path to the input bitmaps")
-    parser.add_argument("--attributes", help="Path to the additional input attributes")
-    parser.add_argument("--labels", help="Path to the input labels")
-    parser.add_argument("--plot", help="Path to the generated history plot image")
-    parser.add_argument("--scalers", help="Path to scalers")
+    parser.add_argument("-b", "--bitmaps", help="Path to the input bitmaps", required=True)
+    parser.add_argument("-a", "--attributes", help="Path to the additional input attributes", required=True)
+    parser.add_argument("-l", "--labels", help="Path to the input labels", required=True)
+    parser.add_argument("-p", "--plot", help="Path to the generated history plot image")
+    parser.add_argument("-s", "--scalers", help="Path to scalers")
+    parser.add_argument("-m", "--model", help="Path to created model")
     args = parser.parse_args()
 
     # load all data and path to scalers if given
     bitmaps = np.load(args.bitmaps)
-    n_samples = round(len(bitmaps) * 0.01)
+    n_samples = round(len(bitmaps) * 0.9)
     bitmaps = bitmaps[:n_samples]
     attributes = np.load(args.attributes)[:n_samples]
     labels = np.load(args.labels)[:n_samples]
@@ -57,7 +73,6 @@ def main():
         (8, 8, 12),
         (15,),
         optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
-        metrics=["mean_squared_error", "binary_accuracy"],
         path_to_scalers=path_to_scalers,
     )
 
@@ -66,9 +81,13 @@ def main():
         [train_target_eval, train_target_mate, train_target_is_mate],
         [val_bitmaps, val_attributes],
         [val_target_eval, val_target_mate, val_target_is_mate],
-        10,
-        256,
+        100,
+        512,
     )
+
+    if args.model is not None:
+        chess_eval.save_model(args.model)
+
     chess_eval.plot_history(
         history,
         args.plot.format(
@@ -81,9 +100,9 @@ def main():
         [test_target_eval, test_target_mate, test_target_is_mate],
     )
 
-    print("MSE on inverse test eval: {}".format(mse_eval))
-    print("MSE on inverse test mate: {}".format(mse_mate))
-    print("Accuracy on test is_mate: {}".format(accuracy_is_mate))
+    print("RMS on inverse test eval: {}".format(np.round(np.sqrt(mse_eval), 1)))
+    print("RMS on inverse test mate: {}".format(np.round(np.sqrt(mse_mate), 3)))
+    print("Accuracy on test is_mate: {}".format(np.round(accuracy_is_mate, 4)))
 
 
 if __name__ == "__main__":